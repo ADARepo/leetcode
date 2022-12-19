80de89ecb76ba81508de167f60e96e3dc269d4e3
Austin <goddemraw@gmail.com>
2022-12-19 11:57:54 -0500

    Final hashmap solution to #1 TwoSum.

--- 1. TwoSum/TwoSum.java
+++ 1. TwoSum/TwoSum.java
@@ -1,32 +1,38 @@
 import java.util.Arrays;
+import java.util.HashMap;
+
 class TwoSum {
     public int[] twoSum(int[] nums, int target) 
     {
+        // Key = the value that the index holds, value = index.
+        HashMap <Integer, Integer> vals = new HashMap<Integer, Integer>();
         int [] ret = new int [2];
-        int sum = 0;
+        int diff;
 
+        // i will be the current index we are checking.
         for (int i = 0; i < nums.length; i++)
         {
-            for (int j = 0; j < nums.length; j++)
+            // diff is the value we need to look for in the hashmap.
+            diff = target - nums[i];
+            
+            // If we have diff as a key, get index and return indices as ret.
+            if (vals.get(diff) != null)
             {
-                if (j == i) continue;
-                if (nums[i] + nums[j] == target)
-                {
-                    ret[0] = i;
-                    ret[1] = j;
-                    return ret;
-                }
+                ret[0] = i;
+                ret[1] = vals.get(diff);
+                return ret;
             }
+            vals.put(nums[i], i);
         }
 
         
-        return ret;
+        return null;
     }
 
     public static void main (String [] args)
     {
         TwoSum a = new TwoSum();
-        int [] theArray = new int[] {3,2,4};
+        int [] theArray = new int[] {3, 2, 4};
 
         System.out.println(Arrays.toString(a.twoSum(theArray, 6)));
     }
